7c7feabcfc27dfefb584cdcfd8afbccd8a1d6c87
Ionut Pistol <ionut.pistol@gmail.com>
2016-06-05 20:12:27 +0300

    Identify bold words in text

--- contextual-sumarries.js
+++ contextual-sumarries.js
@@ -1,12 +1,3 @@
-// Test cases:
-// 	- valid use case - no overlaping
-// 	- nr of words is big (second parameter)
-// 	- overlaping of the words
-//  - bold word position < 5 beginnig of sentence
-//  - bold word position < 5 end of sentence
-//  - bold word middle of sentence
-//  - bold word1 and word2 on difference < 5
-
 function parseInput(input) {
 	var results = [];
 	var positions = input.split('\n');
@@ -19,16 +10,30 @@ function parseInput(input) {
 }
 
 // Input [[6,1],[9,3],[23,5]]
-function checkForOverlappings(list) {
+function identifyBoldWords(list) {
+	var results = [];
+	for (var i = 0; i < list.length; i++) {
+		var positions = parseInt(list[i][0]) + parseInt(list[i][1]);
+		var pos,
+			start = parseInt(list[i][0]);
+		for(var j = start; j < positions; j++) {
+			if (results.indexOf(j) < 0) {
+				results.push(j);
+			}
+		}		
+	}
 
+	return results.sort(function(a, b) {
+		return a-b;
+		});
 }
 
 function writeContext(list, url) {
 	var listOfPositions = parseInput("6,1\n8,1\n23,2");
 
-	var listOfWords = checkForOverlappings(listOfPositions);
+	var positionsOfBoldWords = identifyBoldWords(listOfPositions);
 
-	console.log(listOfPositions);
+	console.log(positionsOfBoldWords);
 }
 
 writeContext("Fill your list", 'test1.html')
